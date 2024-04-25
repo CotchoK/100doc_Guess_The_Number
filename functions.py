ce1296f75a1e27d4fa7a741d2d42b0a9504bf69a
Con <ckritikos@gmail.com>
2024-04-25 14:27:08 +1000

    Reduce repetitive code via functions. Clean up game play loop

--- functions.py
+++ functions.py
@@ -11,17 +11,51 @@ def cls():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 
-def compare(guess, num):
+def compare(guess, num, lives):
     """
     Conducts a comparison between the user's guess and the number selected by the computer
     :param guess: the user's guess passed as a parameter
     :param num: the computer selected number the user is attempting to guess
-    :return: string that will be used in conjunction with a dictionary including those options for outputs
+    :param lives: the number of guesses/lives the player has remaining
+    :return: a string based on the result of the comparison check
+            ,boolean to update game loop
+            ,number of guesses remaining
     """
-    if guess == num:
-        return "win"
-    elif guess > num:
-        return "higher"
-    else:
-        return "lower"
 
+    # these are preset responses base on comparison conditions (could hard code in return statements but experimenting
+    # with dictionaries)
+    response = {
+        "win": "You win",
+        "higher": "Too High",
+        "lower": "Too Low",
+        "lose": "You have run out of guesses. You lose",
+    }
+
+    if lives > 1:
+        if guess == num:
+            return response['win'], True, lives
+        else:
+            lives -= 1
+            if guess > num:
+                return response["higher"], False, lives
+            elif guess < num:
+                return response["lower"], False, lives
+    else:
+        return response["lose"], True, lives
+
+def difficulty():
+    """
+    Asks the user to enter the difficulty they want to play the game
+    :return: difficulty level of the game
+    """
+    mode = ''
+    # loop until user provides valid response to difficulty selection
+    while mode != 'easy' and mode != 'hard':
+
+        mode = input("Please enter either 'easy' or 'hard' for difficulty: ")
+
+        # user selects difficulty
+        if mode == 'easy':
+            return 10
+        elif mode == 'hard':
+            return 5
--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import functions as f
 
 def gameloop():
     """
-    This is the game play loop that will excute and continue running as long as the user has requested to.
+    This is the game play loop that will execute and continue running as long as the user has requested to.
     :return:
     """
     # some ascii art for title
@@ -18,25 +18,11 @@ def gameloop():
 
     # gameplay variables
     number = r.randint(1, 100)  # number the user is to guess
-    mode = ''  # mode variable to store difficulty selection
-    n_guesses = 0  # number of guess variable that initialises to 0
     is_game_over = False  # boolean to control while loop of the game logic
-    responses = {
-        "win": "You win",
-        "higher": "Too High",
-        "lower": "Too Low",
-    }
-    # print(number)
+    print(number)
 
-    # loop until user provides valid response to difficulty selection
-    while mode != 'easy' and mode != 'hard':
-        # user selects difficulty
-        mode = input("Please enter either 'easy' or 'hard' for difficulty: ")
-
-        if mode == 'easy':
-            n_guesses = 10
-        elif mode == 'hard':
-            n_guesses = 5
+    # define the number of guesses based on difficulty selected
+    n_guesses = f.difficulty()
 
     # I'm thinking of a number between 1 and 100.
     print("I'm thinking of a number between 1 & 100")
@@ -44,27 +30,22 @@ def gameloop():
     # loop through execution of code until user guesses the number or user runs out of guesses
     while not is_game_over:
         # notify the user of the number of guesses remaining
-        if n_guesses == 0:
-            print(f"You have run out of guesses. You lose")
-            is_game_over = True
-        else:
-            print(f"You have {n_guesses} attempts remaining to guess the number.")
+        print(f"You have {n_guesses} attempts remaining to guess the number.")
 
         # ask for the user to enter a guessed number
         user_guess = int(input("Make a guess: "))
 
-        result = f.compare(user_guess, number)
-        print(responses[result])
-        if result == 'win':
-            is_game_over = True
-        else:
-            n_guesses -= 1
-            if n_guesses == 0:
-                print(f"You have run out of guesses. You lose")
-                is_game_over = True
-            else:
-                print("Guess Again.")
+        # will conduct checking the users guess against the computers and update variables based on outcome
+        result, is_game_over, n_guesses = f.compare(user_guess, number, n_guesses)
 
+        # print the text string as a result of the comparison
+        print(result)
+
+        # will print Guess Again as long as the game isn't over and that the user still has guesses remaining
+        if n_guesses > 1 and not is_game_over:
+            print("Guess Again.")
+
+        # will check if the game is over and if so will prompt the user if they want to play again or not.
         if is_game_over:
             response = input("Would you like to play again 'y' or anything else to quit: ")
             if response == 'y':
